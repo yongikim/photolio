6b3d9d2dd8a234ce535c6c45a17c0550a3e508a4
yongikim <yongi.kim08@gmail.com>
2024-05-12 17:30:33 +0900

    typo: _blank

--- src/components/OpenInNew.tsx
+++ src/components/OpenInNew.tsx
@@ -4,7 +4,7 @@ import { MdOutlineOpenInNew } from "react-icons/md";
 export const OpenInNew = ({ photoId }: { photoId: string }) => {
   const photoUrl = `/photos/${photoId}`;
   return (
-    <Link href={photoUrl} target="blank">
+    <Link href={photoUrl} target="_blank">
       <MdOutlineOpenInNew
         className="h-6 w-6 cursor-pointer text-white"
       />
